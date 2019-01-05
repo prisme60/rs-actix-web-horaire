0bac75464ff096ab78dd5b400a8b2ebdd6246274
Christian FOUCHER <christian.foucher@gmail.com>
2019-01-05 14:07:24 +0100

    Improve formatting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-extern crate actix_web;
-use actix_web::{server, App, Result, Path, HttpRequest, HttpResponse, Responder};
-
-extern crate horaire;
-
-use horaire::timelines::TimeLine;
-use horaire::source::transilien::transilien;
-use horaire::source::sncf::sncf;
-use horaire::source::ratp::ratp;
-// use horaire::errors::*;
+use actix_web::{server, App, HttpRequest, HttpResponse, Path, Responder, Result};
+use horaire::{
+    source::{ratp::ratp, sncf::sncf, transilien::transilien},
+    timelines::TimeLine,
+};
 
 fn get_time_lines_html<'a, I>(time_lines: I) -> String
 where
     I: Iterator<Item = &'a TimeLine>,
 {
-    let mut strings = time_lines.fold(String::from("<html><head><meta charset=\"UTF-8\"></head><body>"), |acc, ref mut time_line| {
-                        acc + &format!("{}<p>", time_line)
-                    });
+    let mut strings = time_lines.fold(
+        String::from("<html><head><meta charset=\"UTF-8\"></head><body>"),
+        |acc, ref mut time_line| acc + &format!("{}<p>", time_line),
+    );
     strings.pop();
     strings.push_str("</body></html>");
     strings
@@ -41,23 +37,24 @@ fn rt_sncf_dest(info: Path<(String)>) -> String {
 fn rt_sncf_arriv(req: HttpRequest) -> impl Responder {
     match req.match_info().get("station") {
         Some(station) => match sncf(station, false) {
-                    Ok(time_lines) => HttpResponse::Ok()
-                       .content_type("text/html")
-                       .body(get_time_lines_html(time_lines.iter())),
-                    _ => HttpResponse::NotAcceptable().finish()
+            Ok(time_lines) => HttpResponse::Ok()
+                .content_type("text/html")
+                .body(get_time_lines_html(time_lines.iter())),
+            _ => HttpResponse::NotAcceptable().finish(),
         },
-        None => HttpResponse::BadRequest().finish()
+        None => HttpResponse::BadRequest().finish(),
     }
 }
 
 fn main() {
-
-    server::new(
-        || App::new()
-            .resource("/transilien/{station}",  |r| r.with(rt_transilien))
+    server::new(|| {
+        App::new()
+            .resource("/transilien/{station}", |r| r.with(rt_transilien))
             .resource("/ratp/{line}/{station}", |r| r.with(rt_ratp))
-            .resource("/sncf/dest/{station}",   |r| r.with(rt_sncf_dest))
-            .resource("/sncf/arriv/{station}",  |r| r.with(rt_sncf_arriv)))
-        .bind("127.0.0.1:8080").unwrap()
-        .run();
+            .resource("/sncf/dest/{station}", |r| r.with(rt_sncf_dest))
+            .resource("/sncf/arriv/{station}", |r| r.with(rt_sncf_arriv))
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run();
 }
